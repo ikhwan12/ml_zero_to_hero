0f4577733781f8cfc53711028bea2c2996a4c0cd
Ikhwan Mohammad Iqbal <ikhwan.ikhwan52@gmail.com>
2020-04-08 07:40:32 +0900

    handle missing data

--- Chapter 2 Data Preprocessing/data-preprocessing-template.py
+++ Chapter 2 Data Preprocessing/data-preprocessing-template.py
@@ -7,7 +7,6 @@ Created on Mon Apr  6 16:05:55 2020
 
 #Data Preprocessing
 #Importing the libraries
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -16,3 +15,11 @@ dataset = pd.read_csv("data.csv")
 
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,3].values
+
+#Handle Missing Data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN', strategy='mean',axis=0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+#Deal with Categorical Data

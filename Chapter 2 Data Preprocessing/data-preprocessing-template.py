55e5bf3b734424b1e7c4d85959433074fdbc2f8f
Ikhwan Mohammad Iqbal <ikhwan.ikhwan52@gmail.com>
2020-04-09 07:06:50 +0900

    categorical data and dataset splitting

--- Chapter 2 Data Preprocessing/data-preprocessing-template.py
+++ Chapter 2 Data Preprocessing/data-preprocessing-template.py
@@ -23,3 +23,14 @@ imputer = imputer.fit(X[:,1:3])
 X[:,1:3] = imputer.transform(X[:,1:3])
 
 #Deal with Categorical Data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+#Splitting Dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=0)
